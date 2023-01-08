5890a110eacfbf0a92c4c23dd9eaf8c96581dfdb
Husseiny10 <95244061+Husseiny10@users.noreply.github.com>
2023-01-08 20:41:16 +0200

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿Create proc Sp_Login
+@Admin_id nvarchar(100),
+@Password nvarchar(100),
+@IsValid bit out 
+as 
+begin
+Set @IsValid = (select count(1)from tbl_login where Admin_id = @Admin_id and Ad_Password = @Password )
+end 
